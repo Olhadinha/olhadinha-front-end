81d6f53a0f17514a1d96468be80999f9441f8ed4
Giovanny Cordeiro <Giovannycordeiropb@gmail.com>
2023-08-25 16:38:38 -0300

    Feat: Adding Header in main page

--- src/pages/root/index.tsx
+++ src/pages/root/index.tsx
@@ -1,7 +1,12 @@
+import Header from "../../components/Header";
+
 export default function RootPage() {
     return (
         <>
-            <h2 className="">Main page</h2>
+            <Header />
+            <main>
+                <h2 className="">Main page</h2>
+            </main>
         </>
     );
 }
