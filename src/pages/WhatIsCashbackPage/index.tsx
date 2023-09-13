3410370c36167185ac627722221f09ff1b379ad8
Will Souza <WillansF22@gmail.com>
2023-09-13 17:49:33 -0300

    Feat: add title

--- src/pages/WhatIsCashbackPage/index.tsx
+++ src/pages/WhatIsCashbackPage/index.tsx
@@ -1,6 +1,11 @@
+import Header from "../../components/Header";
+
 export default function WhatIsCashbackPage() {
+    document.title = "O que é cashback?";
+
     return (
         <>
+            <Header />
             <h2>What is cashback page</h2>
         </>
     );
